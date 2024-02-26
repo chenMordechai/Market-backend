ffc63e87528c6ddb82b8a6b402ca70a73aefd2c6
Chen <chen100030@gmail.com>
2024-02-26 15:31:11 +0200

    build

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,6 @@
+const pass = process.env.MONGO_PASS
+
 export default {
-    dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-    dbName: 'tester_db',
+    dbURL: `mongodb+srv://chen:${pass}@cluster1.h2v9iky.mongodb.net/?retryWrites=true&w=majority`,
+    dbName: 'market',
 }
